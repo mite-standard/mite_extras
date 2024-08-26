efda630d183d1976ecefd79c97963f7763179be5
Adriano Rutz <adriano.rutz@hotmail.com>
2024-08-26 21:04:57 +0200

    less stringent matches between predicted products and expectations

--- src/mite_extras/processing/validation_manager.py
+++ src/mite_extras/processing/validation_manager.py
@@ -391,10 +391,19 @@ class ValidationManager(BaseModel):
             raise ValueError("One or more predicted products could not be parsed.")
 
         # Check products meet expectations
-        if not expected_smiles_set.issubset(predicted_smiles_set):
-            raise ValueError(
-                f"Products '{predicted_smiles_set}' do not meet expectations '{expected_smiles_set}'."
-            )
+        # TODO add tests
+        # TODO @MMZ tell me if you want to externalize it
+        for expected_smiles in expected_smiles_set:
+            expected_mol = MolFromSmiles(expected_smiles)
+            if not any(MolFromSmiles(predicted_smiles).HasSubstructMatch(expected_mol)
+                       for predicted_smiles in predicted_smiles_set):
+                raise ValueError(
+                    f"Products '{predicted_smiles_set}' do not meet expectations '{expected_smiles_set}'."
+                )
+        # if not expected_smiles_set.issubset(predicted_smiles_set):
+        #     raise ValueError(
+        #         f"Products '{predicted_smiles_set}' do not meet expectations '{expected_smiles_set}'."
+        #     )
 
         # Check if any forbidden products are present in the predicted products
         if forbidden_smiles_set.intersection(predicted_smiles_set):
