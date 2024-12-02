48e6f22e61a4f1f5b6ba3664db223749b1153b3f
Adriano Rutz <adriano.rutz@hotmail.com>
2024-12-02 10:56:07 +0100

    more detailed errors, fixed `logger.critical("passed response")`

--- src/mite_extras/processing/validation_manager.py
+++ src/mite_extras/processing/validation_manager.py
@@ -357,14 +357,20 @@ class ValidationManager(BaseModel):
                 "https://sparql.uniprot.org/sparql",
                 params={"query": query, "format": "srj"},
                 headers={"Accept": "application/sparql-results+json"},
-                timeout=2,
+                timeout=10,
             )
             if not response.ok:
-                raise ValueError("Failed to fetch data from UniProt SPARQL endpoint")
-            results = response.json().get("results", {}).get("bindings", [])
-            if not results:
-                raise ValueError("No matching results found")
-            return results[0]["protein"]["value"].rsplit("/", 1)[-1]
+                raise ValueError(f"HTTP Error: {response.status_code} - {response.reason}")
+            response_json = response.json()
+            bindings = response_json.get("results", {}).get("bindings", [])
+            if not bindings:
+                raise ValueError("No results found in the response")
+            protein_data = bindings[0].get("protein")
+            if not protein_data or "value" not in protein_data:
+                raise ValueError("'protein' key or its 'value' is missing in the response")
+
+            protein_uri = protein_data["value"]
+            return protein_uri.rsplit("/", 1)[-1]
 
         def build_genpept_query(genpept: str) -> str:
             return f"""
@@ -386,7 +392,7 @@ class ValidationManager(BaseModel):
             WHERE {{
                 VALUES ?prefix {{<http://purl.uniprot.org/database/EMBL>}}
                 <http://purl.uniprot.org/uniprot/{uniprot}> rdfs:seeAlso ?protein .
-                ?protein up:database ?prefix .            logger.critical("passed response")
+                ?protein up:database ?prefix .
             }}
             """
 
